769c078777ad258cbb699c63cde168ba671f937c
unknown <nbaniya314@gmail.com>
2024-03-07 20:49:38 +0545

    feat:break-and-continue-is-completed

---
+++ 10_break-and-continue-in-js/break-and-continue.js
@@ -0,0 +1,32 @@
+// Break and Continue
+// The "Break" statement jumps out of the loop
+// The "Continue" Statement jumps over one iteration in the loop
+
+// Break Statement
+
+for(let i = 1; i<= 10; i++){
+    if(i === 8){
+        document.write("The numbers is: ", i + "<br>");
+        break;
+    }
+    document.write("After: ", i +  "<br>")
+}
+
+// continue Statement
+
+for (let j = 1; j<= 5; j++ ){
+    if(j === 3){
+        document.write("The number is: ", j + "<br>")
+        continue;
+    }
+    document.write("After: ", j + "<br>")
+}
+
+// Example: Make the loop stop when i = 5
+
+for (let k = 0; k < 10; k++){
+    document.write(k + "<br>")
+    if( k === 5){
+        break;
+    }
+}
